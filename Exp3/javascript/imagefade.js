ed074f66813051b7cea9ba1b7fb35e917c896f7f
Yash Kulthe <yashkulthex@gmail.com>
2020-03-04 10:13:58 +0530

    Home page
    Images fading Complete

--- Exp3/javascript/imagefade.js
+++ Exp3/javascript/imagefade.js
@@ -1,58 +1,115 @@
 $(document).ready(function(){
     
-    $(".image1").hover(function(){
-        $(".image1").fadeOut(3000);
-      },
-      function(){
-        $(".image1").fadeIn(3000);
-      }); 
+    $(".image1").mouseenter(function(){
+        $(".image1").fadeTo("slow",0.15);
+    });
+    $(".image1").mouseleave(function(){
+        $(".image1").fadeTo("slow",1.0);
+    });
 
     $(".image2").mouseenter(function(){
         $(".image2").fadeTo("slow",0.15);
+    });
+    $(".image2").mouseleave(function(){
+        $(".image2").fadeTo("slow",1.0);
     });
 
     $(".image3").mouseenter(function(){
         $(".image3").fadeTo("slow",0.15);
     });
+    $(".image3").mouseleave(function(){
+        $(".image3").fadeTo("slow",1.0);
+    });
 
     $(".image4").mouseenter(function(){
         $(".image4").fadeTo("slow",0.15);
     });
+    $(".image4").mouseleave(function(){
+        $(".image4").fadeTo("slow",1.0);
+    });
+
     $(".image5").mouseenter(function(){
         $(".image5").fadeTo("slow",0.15);
     });
+    $(".image5").mouseleave(function(){
+        $(".image5").fadeTo("slow",1.0);
+    });
+
     $(".image6").mouseenter(function(){
         $(".image6").fadeTo("slow",0.15);
     });
+    $(".image6").mouseleave(function(){
+        $(".image6").fadeTo("slow",1.0);
+    });
+
     $(".image7").mouseenter(function(){
         $(".image7").fadeTo("slow",0.15);
     });
+    $(".image7").mouseleave(function(){
+        $(".image7").fadeTo("slow",1.0);
+    });
+
     $(".image8").mouseenter(function(){
         $(".image8").fadeTo("slow",0.15);
     });
+    $(".image8").mouseleave(function(){
+        $(".image8").fadeTo("slow",1.0);
+    });
+
     $(".image9").mouseenter(function(){
         $(".image9").fadeTo("slow",0.15);
     });
+    $(".image9").mouseleave(function(){
+        $(".image9").fadeTo("slow",1.0);
+    });
+
     $(".image10").mouseenter(function(){
         $(".image10").fadeTo("slow",0.15);
     });
+    $(".image10").mouseleave(function(){
+        $(".image10").fadeTo("slow",1.0);
+    });
+
     $(".image11").mouseenter(function(){
         $(".image11").fadeTo("slow",0.15);
     });
+    $(".image11").mouseleave(function(){
+        $(".image11").fadeTo("slow",1.0);
+    });
+
     $(".image12").mouseenter(function(){
         $(".image12").fadeTo("slow",0.15);
     });
+    $(".image12").mouseleave(function(){
+        $(".image12").fadeTo("slow",1.0);
+    });
+
     $(".image13").mouseenter(function(){
         $(".image13").fadeTo("slow",0.15);
     });
+    $(".image13").mouseleave(function(){
+        $(".image13").fadeTo("slow",1.0);
+    });
+
     $(".image14").mouseenter(function(){
         $(".image14").fadeTo("slow",0.15);
     });
+    $(".image14").mouseleave(function(){
+        $(".image14").fadeTo("slow",1.0);
+    });
+
     $(".image15").mouseenter(function(){
         $(".image15").fadeTo("slow",0.15);
     });
+    $(".image15").mouseleave(function(){
+        $(".image15").fadeTo("slow",1.0);
+    });
+
     $(".image16").mouseenter(function(){
         $(".image16").fadeTo("slow",0.15);
+    });
+    $(".image16").mouseleave(function(){
+        $(".image16").fadeTo("slow",1.0);
     });
 });
 
