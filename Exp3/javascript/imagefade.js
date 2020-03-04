0106afdf48e303affa3cb12653ec009e1dd76389
Yash Kulthe <yashkulthex@gmail.com>
2020-03-04 08:06:38 +0530

    Home Page 3

---
+++ Exp3/javascript/imagefade.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    
+    $(".image1").hover(function(){
+        $(".image1").fadeOut(3000);
+      },
+      function(){
+        $(".image1").fadeIn(3000);
+      }); 
+
+    $(".image2").mouseenter(function(){
+        $(".image2").fadeTo("slow",0.15);
+    });
+
+    $(".image3").mouseenter(function(){
+        $(".image3").fadeTo("slow",0.15);
+    });
+
+    $(".image4").mouseenter(function(){
+        $(".image4").fadeTo("slow",0.15);
+    });
+    $(".image5").mouseenter(function(){
+        $(".image5").fadeTo("slow",0.15);
+    });
+    $(".image6").mouseenter(function(){
+        $(".image6").fadeTo("slow",0.15);
+    });
+    $(".image7").mouseenter(function(){
+        $(".image7").fadeTo("slow",0.15);
+    });
+    $(".image8").mouseenter(function(){
+        $(".image8").fadeTo("slow",0.15);
+    });
+    $(".image9").mouseenter(function(){
+        $(".image9").fadeTo("slow",0.15);
+    });
+    $(".image10").mouseenter(function(){
+        $(".image10").fadeTo("slow",0.15);
+    });
+    $(".image11").mouseenter(function(){
+        $(".image11").fadeTo("slow",0.15);
+    });
+    $(".image12").mouseenter(function(){
+        $(".image12").fadeTo("slow",0.15);
+    });
+    $(".image13").mouseenter(function(){
+        $(".image13").fadeTo("slow",0.15);
+    });
+    $(".image14").mouseenter(function(){
+        $(".image14").fadeTo("slow",0.15);
+    });
+    $(".image15").mouseenter(function(){
+        $(".image15").fadeTo("slow",0.15);
+    });
+    $(".image16").mouseenter(function(){
+        $(".image16").fadeTo("slow",0.15);
+    });
+});
+
